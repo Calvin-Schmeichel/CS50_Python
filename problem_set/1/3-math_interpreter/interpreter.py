ec30641511e0f5b46c2e9b8fe172f344a32b876c
Calvin-Schmeichel <93797574+Calvin-Schmeichel@users.noreply.github.com>
2026-04-19 11:57:35 -0500

    completed Math Interpreter

--- problem_set/1/3-math_interpreter/interpreter.py
+++ problem_set/1/3-math_interpreter/interpreter.py
@@ -1,4 +1,13 @@
-# user_string = input("Expression: ")
-user_string = "1 + 1"
+user_string = input("Expression: ")
 
-print(user_string.split())
+# Simple Input Validation: Only allow numbers, operators, spaces and variables x, y, z.
+# This is not a secure way to evaluate expressions, but it is sufficient for this problem set.
+valid_chars = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '*', '/', '+', '-', 'x', 'y', 'z', ' ')
+
+# Checks if the user_string set containes any characters that are not in the valid_chars set.
+if set(user_string) - set(valid_chars):
+    # If it does, it prints "Invalid Input!" Otherwise,
+    print("Invalid Input!")
+else:
+    # It evaluates the expression using eval() and prints the result as a float.
+    print(float(eval(user_string)))
